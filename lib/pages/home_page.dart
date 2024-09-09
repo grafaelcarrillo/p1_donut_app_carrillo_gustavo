c018cdb0214fc04a063cdf854058cc7d51fcc503
Gustavo <grafaelcarrillo@gmail.com>
2024-09-09 13:39:41 -0600

    íconos añadidos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p1_donut_app_carrillo_gustavo/utils/my_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +9,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<Widget> myTabs = [
+    //donuttab
+    const MyTab(iconPath: "lib/icons/donut.png",),
+    //burgertab
+    const MyTab(iconPath: "lib/icons/burger.png",),
+    //smoothietab
+    const MyTab(iconPath: "lib/icons/smoothie.png",),
+    //pancaketab
+    const MyTab(iconPath: "lib/icons/pancakes.png",),
+    //pizzatab
+    const MyTab(iconPath: "lib/icons/pizza.png",),
+  ]; 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        leading: const Icon(Icons.menu, color: Colors.white,)
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: Icon(
+              Icons.menu, 
+              color: Colors.grey[800],
+              ),
+          ),
+        actions: const [Padding(
+          padding: EdgeInsets.only(right:24.0),
+          child: Icon(Icons.person),
+          )],
+        ),
+        body: Column(children:[
+          //texto i want to eat
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              children: [
+                Text("I want to ", style: TextStyle(fontSize:24)),
+                Text("Eat", style: TextStyle(fontSize: 24, fontWeight:FontWeight.bold, decoration: TextDecoration.underline)),
+              ],
+            ),
+          ),
+          //tabbar
+          TabBar(tabs: myTabs),
+      
+          //total del carrito
+        ],),
       ),
     );
   }
